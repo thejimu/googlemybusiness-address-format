08d30e0feedd80bb6b00ab896e21afe632f82005
James <james@beardybanana.com>
2020-03-11 08:46:09 +0800

    First commit! The function!

---
+++ format.php
@@ -0,0 +1,23 @@
+<?php
+
+public function addressFormat($addressObject)
+    {
+        $addressArray=[];
+        if($addressObject['addressLines']!=null) {
+            foreach ($addressObject['addressLines'] as $item) {
+                array_push($addressArray, $item);
+            }
+
+            array_push($addressArray, $addressObject['locality'] ?? "");
+            array_push($addressArray, $addressObject['administrativeArea'] ?? "");
+            array_push($addressArray, $addressObject['postalCode'] ?? "");
+
+            if ($addressObject['languageCode'] == 'zh-TW') {
+                $addressString = implode("", array_reverse($addressArray));
+            } else {
+                $addressString = implode(" ", $addressArray);
+            }
+        }
+
+        return $addressString ?? "";
+     }
